16fa78b6f68c31951d7a8252714058f2ddb6feb1
Sethorvo <86128743+Sethorvo@users.noreply.github.com>
2022-12-07 23:06:36 -0500

    Nouveau push
    Il y'a 4-5 colonnes qui marchent pas encore pour l'importation mais sinon pour la majorité les données sont importés et en filtrant les labels avec des regex, pandas associent automatiquement les valeurs aux bonnes rangées donc ça imprime un tableau presque correcte

--- Devoir 2.py
+++ Devoir 2.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
-#Importe le premier facile, je le garde comme exemple
-
+#Importe le premier facile, je le garde comme fonction de test pour imprimer une colonne
 
 # url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita"
 # table = pd.read_html(url)[1]
 # colonne = table[table.columns[6]]
-# print(colonne)
+# colonne = colonne.truncate(1,223)
+# colonne.index = table[table.columns[0]].truncate(1,223)
+# colonne.index = colonne.index.str.replace('[^a-zA-Z]', '', regex=True)
 
 
 
@@ -35,26 +36,32 @@ url_array = ["https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_p
              "https://en.wikipedia.org/wiki/List_of_countries_by_food_energy_intake","https://en.wikipedia.org/wiki/Books_published_per_country_per_year",
              "https://en.wikipedia.org/wiki/List_of_countries_by_food_energy_intake","https://en.wikipedia.org/wiki/List_of_countries_by_average_yearly_temperature",
 ]
-
+#Pandas lit une page wiki comme une serie de dataframe, c'est l'index du df que l'on veut
 table_array = [1,0,0,1,4,1,5,1,4,7,
 3,0,34,0,1,2,0,1,0,1,
 2,0,0,5,0,1,0,0,1,1,
 0,1,0,0,0,0,0,0,0,0]
+
+#Indice de la colonne que l'on veut
 colonne_array = [6,2,2,3,2,3,5,1,2,2,
-3,2,1001,1,1,2,4,7,2,1,
-2,1,1,1001,3,2,2,6,1,1,
+3,2,-1,1,1,2,4,7,2,1,
+2,1,1,-1,3,2,2,6,1,1,
 3,5,2,1,3,3,3,3,2,1]
 
-label_u = [1,4,8,20,25,27,36,37,38]
+#Indice des liens ou les noms de pays ne sont pas dans la colonne 0, on s'en sert pour les labels
+label_u = [1,4,8,20,25,36,37,38]
 label_d = [5,6,35]
 
 
 
 def get_colonnes() :
+    
     df = pd.DataFrame()
+    #Prend la liste des pays et territoire de la page du pib commme label de depart pour les range
     table = pd.read_html(url_array[0])[1]
-    labels = table[table.columns[0]]
-    labels = labels.truncate(1,224)
+    labels = table[table.columns[0]].truncate(1,223)
+    labels = labels.str.replace('\s.+', '', regex=True)
+    labels = labels.truncate(1)
     df.index = labels
 
 
@@ -63,13 +70,14 @@ def get_colonnes() :
         table_i = table_array[i]
         colonne_i = colonne_array[i]
 
-
-        if colonne_i == 1001:
+        #Saute les deux liens ou je n'arrive a pas a obtenir les colonnes
+        if colonne_i == -1:
             continue
 
         else :
-            table = pd.read_html(url, na_values="-")[table_i]
+            table = pd.read_html(url)[table_i]
 
+            #Va chercher le nom des pays pour la colonne que l'on extrait
             if i in label_u :
                 label = table[table.columns[1]]
 
@@ -88,25 +96,41 @@ def get_colonnes() :
                 colonne = colonne.truncate(3,17)
                 label = label.truncate(3,17)
 
-            if i == 20:
+            elif i == 20:
                 colonne = colonne.truncate(0,201)
                 label = label.truncate(0,201)
 
-            if i == 32:
+            #Ajoute les differents tableaux de la page en un, une des consignes du prof
+            elif i == 32:
+                #Pour l'instant les labels sont pas reconnus comme des strings je sais pas pourquoi, a voir
+                continue
                 url = url_array[i]
                 resultat = []
+                label_r = []
 
                 for j in range(5):
                     afr = pd.read_html(url)[j]
-                    col_df = afr[afr.columns[2]]
-                    col_arr = col_df.to_numpy()
-                    for k in col_arr:
-                        resultat.append(k)
 
+                    lab_df = afr[afr.columns[0]]
+                    col_df = afr[afr.columns[2]]
+
+                    lab_arr = lab_df.to_numpy()
+                    col_arr = col_df.to_numpy()
+
+                    for k in range(len(col_arr)):
+                        label_r.append(lab_arr[k])
+                        resultat.append(col_arr[k])
+                        
                 colonne = pd.DataFrame({'col':resultat})
+                label = pd.DataFrame({'id':label_r})
+                label = label.str.replace('[^a-zA-Z]', '', regex=True)
+                colonne.index = label
+                df[str(i)] = colonne
+                continue
+
             
             colonne.index = label
-            print(df.index.duplicated())
+            colonne.index = colonne.index.str.replace('[^a-zA-Z]', '', regex=True)
             df[str(i)] = colonne
 
     print(df.to_string())
