65168ebf2665df1ed635982086b0fdde2e86a111
Hugo <hugo.carrier@umontreal.com>
2022-12-21 11:30:51 -0500

    linear

--- src/LinearRegression.py
+++ src/LinearRegression.py
@@ -1,26 +1,29 @@
 import pandas as pd
-from sklearn.feature_selection import SelectKBest
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import f_regression
 
 
 def find_best_linear_regression(df: pd.DataFrame):
+    df_means = df.mean() # might not need it
+    df_std =  df.std()
     for column in df.columns:
         y = df[column]
         x = df.loc[:, df.columns != column]
         data_point_x, data_point_x_test, data_point_y, data_point_y_test = train_test_split(x, y, test_size=0.4)
-        regression = LinearRegression().fit(data_point_x, data_point_y)
-        predictions = regression.predict(data_point_x_test)
+        regression = sm.OLS(data_point_y,data_point_x).fit()
+        params_coff = regression.params
+        # regression.summary()
+        # predictions = regression.predict(data_point_x_test)
 
-        fs = SelectKBest(score_func=f_regression, k=2)
-        # learn relationship from training data
-        fs.fit(data_point_x, data_point_y)
-        # transform train input data
-        X_train_fs = fs.transform(data_point_x)
-        # transform test input data
-        X_test_fs = fs.transform(data_point_x_test)
+        #calculer l'impact, donc normalisé
+        impact={}
+        for key in params_coff.keys():
+            impact[key] = params_coff[key]/df_std[key]
 
         continue
 
+
+
     return df
+
+
