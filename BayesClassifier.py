711a05096226dd18b43da7afec19c280a787fd37
Sethorvo <86128743+Sethorvo@users.noreply.github.com>
2022-12-22 19:51:29 -0500

    Changement finals

--- BayesClassifier.py
+++ BayesClassifier.py
@@ -11,14 +11,14 @@ from sklearn import metrics
 # return_matrix[i][j] returns the accuracy score if you use naive bayes to
 # predict column j with column i of data array (countries)
 def get_bayes_prediction_scores(data, split):
-    return_matrix = np.zeros(shape = (data.shape[1],data.shape[1]))
+    return_matrix = np.zeros(shape=(data.shape[1], data.shape[1]))
     nb_test = int(data.shape[0] * split)  # for train / test split
     return_matrix = return_matrix * 0  # set all elements to zero
     bayes_classifier = MultinomialNB()  # initiate multinomial bayes classifier
     for i in range(return_matrix.shape[1]):
         for j in range(return_matrix.shape[1]):
             bayes_classifier.fit(data[0:nb_test, i].reshape(-1, 1), data[0:nb_test, j].reshape(-1, 1))
-            prediction = bayes_classifier.predict(data[nb_test:, j].reshape(-1, 1))
+            prediction = bayes_classifier.predict(data[nb_test:, i].reshape(-1, 1))
             return_matrix[i][j] = metrics.accuracy_score(prediction, data[nb_test:, j])
     return return_matrix
 
@@ -36,14 +36,3 @@ def get_best_two(score_matrix):
     np.fill_diagonal(score_matrix, 0)
     mean_scores = np.mean(score_matrix, axis=1)
     return np.argsort(mean_scores, axis=0)[0:2]
-
-
-def test():
-    countries = np.random.randint(5, size=(4, 8))
-    predictions = get_bayes_prediction_scores(countries, 0.8)
-    print( predictions )
-    print(get_best_pair_for_each(predictions))
-    print(get_best_two(predictions))
-
-
-test()
